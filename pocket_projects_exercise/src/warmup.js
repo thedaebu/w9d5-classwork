25a08cee81bb601f50fe9f993d4be018bbf20161
Kenny Li <kenny.wj.li@gmail.com>
2021-02-26 11:27:27 -0500

    finished clock

--- pocket_projects_exercise/src/warmup.js
+++ pocket_projects_exercise/src/warmup.js
@@ -2,13 +2,21 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
+    if (htmlElement.children){
+        const children = Array.from(htmlElement.children);
+        children.forEach(child =>{
+            htmlElement.removeChild(child)
+        })
+    }
     let header = document.createElement("p" );
 
     const newContent = document.createTextNode(string);
 
     header.appendChild(newContent);
 
-    document.body.insertBefore(header, htmlElement.nextSibling);
+
+    htmlElement.appendChild(header)
+    // document.body.insertBefore(header, htmlElement.nextSibling);
 
 };
 
