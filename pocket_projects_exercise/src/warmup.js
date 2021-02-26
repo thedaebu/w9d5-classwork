c221db33e204f5c1c9a2b14b9764918c4dc01873
thedaebu <min.kim163@gmail.com>
2021-02-26 10:29:56 -0500

    finished the warmup

--- pocket_projects_exercise/src/warmup.js
+++ pocket_projects_exercise/src/warmup.js
@@ -2,6 +2,13 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
+    let header = document.createElement("p" );
+
+    const newContent = document.createTextNode(string);
+
+    header.appendChild(newContent);
+
+    document.body.insertBefore(header, htmlElement.nextSibling);
 
 };
 
